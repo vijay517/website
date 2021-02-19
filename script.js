2bedc098b4bd79d3759f1b5b40460ec06ef8ed6e
vijay517 <72249607+vijay517@users.noreply.github.com>
2021-02-19 21:11:02 +0800

    Changes

--- script.js
+++ script.js
@@ -5,61 +5,90 @@
 function initialiseNavWatcher() {
   const currentPage = window.location.pathname
   const currentPathName = currentPage.split('/')[1]
-  
+
   // all anchor tags that are nav links
   const allNavLinks = document.querySelectorAll(`.navbar-nav .nav-item a`)
-  
+
   allNavLinks.forEach(navLink => {
     // one anchor tag
     const navLinkHref = navLink.href
-    
+
     const pathName = navLinkHref.split('/').pop()
-  
-    if(pathName === currentPathName) {
-  
+
+    if (pathName === currentPathName) {
+
       navLink.parentElement.classList.add('active')
     }
-  
+
   })
 }
 
 initialiseNavWatcher()
 
+const formElement = document.querySelector('#optimisation-form');
 
-const selectedOptmisation = 'SOO' // 'MOO
+//function that gets executed when optimise button is pressed
+function optimise(event) {
+  event.preventDefault();
 
-// const parameters =  {
-//   cement: {
-//     optimised: true,
-//     value: 23,
-//   },
-//   flyAsh: {
-//     optimised: true,
-//     value: 23,
-//   }
-// }
+  const formElement = document.querySelector('#optimisation-form');
+  const data = new FormData(formElement);
 
-function initialiseForm () {
-  const formElement = document.querySelector('#optimisation-form')
-  
-  function optimise(event) {
-    event.preventDefault()
-    console.log('hello optimise')
+  const result = {};
 
-    const allOptimisationTypeInputs = document.querySelectorAll('input[name="optimisationType"]')
-    const selectedOptmisationTypeInput = Array.from(allOptimisationTypeInputs).find(input => {return input.checked})
+  data.forEach((value, key) => {
+    if (key === 'optimisationType') {
+      result.optimisationType = value;
+    }
 
-    console.log('the selectedType: ', selectedOptmisationTypeInput.value)
+    if (key.includes('-optimised')) {
+      const parameter = key.split('-optimised')[0];
 
-    const allInputElements = document.querySelectorAll('input')
+      if (!result[parameter]) {
+        result[parameter] = { optimised: value === 'on' ? true : false };
+      } else {
+        result[parameter].optimised = value === 'on' ? true : false;
+      }
 
-  }
+    }
 
 
-  formElement.addEventListener('submit',optimise)
-  
+    if (key.includes('-value')) {
+      const parameter = key.split('-value')[0];
 
-  
+      if (!result[parameter]) {
+        result[parameter] = { value: value ? parseFloat(value) : null };
+      } else {
+        result[parameter].value = value ? parseFloat(value) : null;
+      }
+      if (!result[parameter].hasOwnProperty('optimised')) {
+        result[parameter].optimised = false;
+      }
+    }
+  });
+
+  console.log(result);
+
 }
-initialiseForm()
 
+formElement.addEventListener('submit', optimise);
+
+
+//Write a function that takes in a json object and fills in the optimisation results table
+
+
+//Write a function that clears the optimisation results table
+
+
+
+//Write a function that takes in a json object to fill in the deployed predictive model section
+
+
+//Write a function that toggles between the deployement status
+
+
+
+//Write a function that takes in a json object to fill in the predictive models section
+
+
+//Write a funtion that returns the model name when pressed deployed.
